85397c424079b0a1aa8581d436b1e7c471953085
Fridolin Somers <fridolin.somers@biblibre.com>
2019-06-14 15:39:51 +0200

    verion 1.0 : just body background color

---
+++ Koha/Plugin/Com/BibLibre/ThemeIntranetLsd.pm
@@ -0,0 +1,48 @@
+package Koha::Plugin::Com::BibLibre::ThemeIntranetLsd;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+use C4::Context;
+
+our $VERSION = '1.0';
+
+our $metadata = {
+    name   => 'Theme Intranet LSD',
+    author => 'BibLibre',
+    description => 'An hallucinating theme for intranet',
+    date_authored   => '2019-06-01',
+    date_updated    => '2019-06-01',
+    minimum_version => '17.1111000',
+    maximum_version => undef,
+    version         => $VERSION,
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+sub intranet_head {
+    my ( $self ) = @_;
+
+    return q|
+<style>
+/*
+ * LSD Intranet Theme
+ */
+body {
+    background-color: turquoise;
+}
+</style>
+|;
+}
+
+1;
